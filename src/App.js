7c0e8d4132c4901e8a918ed7447165656ffb1639
dcaaz <danicorreaazevedo@gmail.com>
2023-03-21 13:35:24 -0300

    starting architecture and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignInPage from "./Pages/SignIn";
+import SignUpPage from "./Pages/SignUp";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<SignInPage />}></Route>
+        <Route path="/up" element={<SignUpPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

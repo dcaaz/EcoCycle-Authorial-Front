50bb50129a2092329165558a126011316063c1bc
dcaaz <danicorreaazevedo@gmail.com>
2023-04-04 20:21:51 -0300

    feat: beginning of the logic behind the google api

--- src/Pages/Maps.js
+++ src/Pages/Maps.js
@@ -1,15 +1,56 @@
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, MarkerF } from '@react-google-maps/api';
 import styled from 'styled-components';
 import { All } from '../Style/Constant/User-Style';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function MapsPage() {
+
+    const [markers, setMarkers] = useState([]);
+
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY
     })
 
-    console.log("isLoaded", isLoaded);
+    /*  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY);
+ 
+     ceps.map(async cep => {
+         Geocode.fromAddress(cep).then(
+             (response) => {
+                 const { lat, lng } = response.results[0].geometry.location;
+                 setMarkers([...markers, { lat, lng }]);
+             },
+             (error) => {
+                 console.error(error);
+             }
+         );
+     })
+     */
 
+     const ceps = ["88111350", "41620840"];
+
+    useEffect(() => {
+
+        ceps.map((c) => {
+            console.log("C", c)
+            const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${c}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`
+
+            const promise = axios.get(url);
+
+            promise.then((res) => {
+                console.log("Deu certo", res.data.results[0].geometry.location);
+                const teste = res.data.results[0].geometry.location;
+                setMarkers([...markers, teste]);
+            })
+
+            promise.catch((err) => {
+                console.log("Deu errado", err)
+            })
+        })
+    }, []);
+
+    console.log("markers", markers);
 
     const containerStyle = {
         width: "100vw",
@@ -30,15 +71,18 @@ export default function MapsPage() {
                         center={position}
                         zoom={19}
                     >
-                        <Marker
-                            position={position}
-                            label="Testando"
-                            options={{
-                                label: {
-                                    text: "Posição Testee"
-                                },
-                            }}
-                        />
+                      {/*  {markers.map((m, i) => (
+                            <MarkerF
+                                key={i}
+                                position={m}
+                                label="Testando"
+                                options={{
+                                    label: {
+                                        text: "Posição teste"
+                                    },
+                                }}
+                            />
+                        ))} */}
                     </GoogleMap>
                 ) : (
                     <>Erro</>
@@ -49,9 +93,9 @@ export default function MapsPage() {
 }
 
 const Google = styled.div`
-    display: flex;
+    /* display: flex;
     align-items: center;
     background-color: red;
     width: 90%;
-    height: 90%;
+    height: 90%; */
 `
